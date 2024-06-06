065c32874aa20c6d4cc1c9fcbea82d068f1d97fa
Namra Vora <124480820+Vora-Namra@users.noreply.github.com>
2024-06-06 10:59:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let tag = document.getElementById("joke");
+let punch = document.getElementById("punch");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click",async function f(){
+    let data = await fetch("https://official-joke-api.appspot.com/jokes/programming/random")
+    let res = await data.json();
+    console.log(res);
+    tag.innerText = res[0].setup;
+    setTimeout(() => {     
+    punch.innerText = res[0].punchline;
+    }, 1000);
+    });
+
+
